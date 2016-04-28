50c41da903511ec1e435df62df2e68eec727f9f9
Fulvio Corno <fulvio.corno@polito.it>
2016-04-28 17:30:57 +0200

    jQuery exercise in classroom

--- static/js/todolist.js
+++ static/js/todolist.js
@@ -1,15 +1,26 @@
-function titleClicked() {
-    alert("Oouch!") ;
-}
+$(document).ready( function() {
 
-function checkForm() {
-    var input = document.getElementById("descriptioninput") ;
-    var text = input.value ;
-    if(text.length != 0)
-        return true ;
-    else {
-        alert("Error: empty text");
-        return false ;
-    }
+    // this code will be executed only the DOM is fully loaded
 
-}
+    $('div.jumbotron').click( function() {
+
+        // code that is executed when che jumbotron is clicked
+        alert("Got you!") ;
+    } ) ;
+
+    $('form').submit( function(event) {
+        // validate the form
+
+        $('div#errorbox').empty() ;
+        var text = $("input[name='description']").val() ;
+
+        if(text.length < 3) {
+            $('div#errorbox').text("Description too short").addClass("text-danger") ;
+            $("input[name='description']").parent().addClass("has-error") ;
+            window.setTimeout( function() {
+                $("input[name='description']").parent().removeClass("has-error") ;
+            } , 1000) ;
+            event.preventDefault() ;
+        }
+    } ) ;
+} ) ;
